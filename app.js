57b51158c63d3a98746f5fb479baf19c6b62291b
StalinNarvaez <stalingsm@hotmail.com>
2023-04-11 02:21:00 +0000

    depuracion

--- app.js
+++ app.js
@@ -1,20 +1,17 @@
+function palabraAleatoria(lista){
+  let aleatorio= Math.floor(Math.random()* lista.length);
+  return lista[aleatorio];
+}
 
-let who = ['The dog','My grandma','His turtle','My bird'];
-let action = ['ate','peed','crushed','broke'];
-let what = ['my homework', 'the keys', 'the car'];
-let when = ['before the class','right on time','when I finished','during my lunch','while I was praying'];
+let listaQuien = ['The dog','My grandma','His turtle','My bird'];
+let listaAccion = ['ate','peed','crushed','broke'];
+let listaQue = ['my homework', 'the keys', 'the car'];
+let listaDonde = ['before the class','right on time','when I finished','during my lunch','while I was praying'];
 
-function quien(){
-  return (Math.floor(Math.random()* who.length));
-}
-function accion(){
-  return (Math.floor(Math.random()* action.length));
-}
-function que (){
-  return Math.floor(Math.random()* what.length);
-}
-function cuando (){
-  return Math.floor(Math.random()* when.length);
-}
-document.getElementById("excuse").innerHTML= who[quien()]+" "+action[accion()]+" "+what[que()]+" "+when[cuando()];
+let quien = palabraAleatoria (listaQuien);
+let accion = palabraAleatoria(listaAccion);
+let que = palabraAleatoria (listaQue);
+let donde = palabraAleatoria(listaDonde);
+
+document.getElementById("excuse").innerHTML= quien+" "+accion+" "+que+" "+donde;
 
